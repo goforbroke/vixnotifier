f93fdc7b5644a64e779f4f512a4d10109aeec184
koba <dk@MBP2019.local>
2020-03-15 16:20:23 +0900

    refactor: get token process.

--- notifyVixFromQuandl/notifyVixFromQuandl/index.js
+++ notifyVixFromQuandl/notifyVixFromQuandl/index.js
@@ -1,32 +1,36 @@
 const https = require('https');
+const VIX_URL = "https://www.quandl.com/api/v3/datasets/CBOE/VIX/data.json?limit=7";
 const TOKEN = require('./token');
 
 exports.handler = async(event, context, callback) => {
 
-  const vix = await parseVix();
-  const result = await postToSlack(vix);
-
+  const vix = await vixParser();
+  vix.forEach(e => console.log(e));
   callback(null, result);
 };
 
-const parseVix = () => new Promise((resolve, reject) => {
+const vixParser = () => new Promise((resolve, reject) => {
 
   let vix = new Map();
   const options = {
-    hostname: TOKEN.QUANDL_HOST,
-    path: TOKEN.QUANDL_PATH_WITH_TOKEN;
+    hostname: 'www.quandl.com',
+    path: `/api/v3/datasets/CBOE/VIX/data.json?limit=14&auth_token=${TOKEN.QUANDL}`,
   }
 
     let body = '';
     const req = https.request(options, (res) => {
       res.on('data', (chunk) => {
         body += chunk;
+
       });
 
       res.on('end', () => {
         let vixJson = JSON.parse(body);
         let element = '';
+        
+        vix.set("Source", "CBOE VIX");
 
+        // pic VIX data
         // i=0:Date, i=1:VIX Open, i=2:VIX High, i=3:VIX Low, i=4:VIX Close
         vixJson.dataset_data.column_names.forEach((v, i) => {
           element = vixJson.dataset_data.data[0][i]
@@ -44,10 +48,9 @@ const parseVix = () => new Promise((resolve, reject) => {
     req.end();
 });
 
-
 const postToSlack = (vixMap) => new Promise((resolve, reject) => {
 
-  // transfer to post json
+  // Transfer for slack post format
   const o = {};
   o.text = '';
 
@@ -59,7 +62,7 @@ const postToSlack = (vixMap) => new Promise((resolve, reject) => {
 
   const options = {
     hostname: TOKEN.SLACK_HOST,
-    path: TOKEN.SLACK_PATH_WITH_TOKEN,
+    path: TOKEN.SLACK_PATH,
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -71,7 +74,6 @@ const postToSlack = (vixMap) => new Promise((resolve, reject) => {
     console.log('STATUS: ' + res.statusCode);
     console.log('HEADERS: ' + JSON.stringify(res.headers));
     res.setEncoding('utf8');
-
     res.on('data', (chunk) => {
       console.log('BODY: ' + chunk);
     });
@@ -86,6 +88,6 @@ const postToSlack = (vixMap) => new Promise((resolve, reject) => {
   });
 
   req.write(vix);
+
   req.end();
 });
-
--- notifyVixFromQuandl/notifyVixFromQuandl/token.js
+++ notifyVixFromQuandl/notifyVixFromQuandl/token.js
@@ -1,7 +1,6 @@
 module.exports = {
-  QUANDL_HOST: 'www.quandl.com',
   // Get API token from https://www.quandl.com/
-  QUANDL_PATH_WITH_TOKEN:  '/api/v3/datasets/CBOE/VIX/data.json?limit=1&auth_token=<set your token>'
+  QUANDL: '<set your quandl token>',
   SLACK_HOST: 'hooks.slack.com',
-  SLACK_PATH_WITH_TOKEN: '/services/<Set slack webhook path with token>',
+  SLACK_PATH: '/services/<Set slack webhook path with token>',
 }
