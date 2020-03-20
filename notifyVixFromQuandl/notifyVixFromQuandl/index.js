e0956bab9c83be2e2c3e558fb112cf3393c194ba
koba <dk@MBP2019.local>
2020-03-20 20:41:26 +0900

    refactor: config, class,

--- notifyVixFromQuandl/notifyVixFromQuandl/index.js
+++ notifyVixFromQuandl/notifyVixFromQuandl/index.js
@@ -4,64 +4,67 @@ const { WebClient } = require('@slack/client');
 
 exports.handler = async (event, context, callback) => {
 
-  // const vix = await vixParser();
-  const vixSeries = await vixParser();
-  const vixPng = drawGraph(vixSeries);
-  // vix.forEach(e => console.log(e));
-  // const result = '';
-  // const result = await postToSlack(vix);
-  const result = await postToSlack(vixPng);
+  const vixSeries = await vixDownload();
+  const graphDrawer = new GraphDrawer(vixSeries);
+  const vixPng = graphDrawer.draw();
+  const todayVIX = vixSeries.data[vixSeries.data.length - 1];
+  const result = await postToSlack(vixPng, todayVIX);
   callback(null, result);
 };
 
-const drawGraph = (vixSeries) => {
-  const { CanvasRenderService } = require('chartjs-node-canvas');
-  const renderService = new CanvasRenderService(800, 600);
-  const options = {
-    plugins: [{
-      beforeDraw: drawBackground
-    }],
-    type: 'line',
-    data: {
-      labels: vixSeries.labels,
-      datasets: [{
-        label: `CBOE VIX: ${vixSeries.labels[0]} - ${vixSeries.labels[vixSeries.labels.length - 1]}`,
-        data: vixSeries.data,
-        backgroundColor:
-          'rgba(255, 99, 132, 0.2)',
-        borderColor:
-          'rgba(255,99,132,1)',
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
+class GraphDrawer {
+
+  // series = { labels: [<string>], data: [<number>] }
+  constructor(series = {}) {
+    this.series = series;
+    const { CanvasRenderService } = require('chartjs-node-canvas');
+    this.renderService = new CanvasRenderService(800, 600);
+    this.startDay = series.labels[0];
+    this.endDay = series.labels[series.labels.length - 1];
+    this.options = {
+      plugins: [{
+        beforeDraw: this.drawBackground
+      }],
+      type: 'line',
+      data: {
+        labels: series.labels,
+        datasets: [{
+          label: `CBOE VIX: ${this.startDay} - ${this.endDay}`,
+          data: series.data,
+          backgroundColor:
+            'rgba(255, 99, 132, 0.2)',
+          borderColor:
+            'rgba(255,99,132,1)',
+          borderWidth: 1
         }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
       }
     }
-  };
-  const buffer = renderService.renderToBufferSync(options);
-  return buffer;
-  // const fs = require('fs');
-  // fs.writeFileSync('test.png', buffer);
+  }
+
+  drawBackground(c) {
+    const ctx = c.chart.ctx;
+    ctx.fillStyle = 'rgba(255,255,255,1)';
+    ctx.fillRect(0, 0, c.chart.width, c.chart.height);
+  }
+
+  draw() {
+    return this.renderService.renderToBufferSync(this.options);
+  }
 }
 
-const drawBackground = (c) => {
-  const ctx = c.chart.ctx;
-  ctx.fillStyle = 'rgba(255,255,255,1)';
-  ctx.fillRect(0, 0, c.chart.width, c.chart.height);
-}
-
-const vixParser = () => new Promise((resolve, reject) => {
-
-  let vix = new Map();
+const vixDownload = () => new Promise((resolve, reject) => {
   const options = {
-    hostname: 'www.quandl.com',
-    path: `/api/v3/datasets/CBOE/VIX/data.json?limit=14&auth_token=${CONFIG.QUANDL}`,
+    hostname: CONFIG.QUANDL_HOST,
+    path: `${CONFIG.QUANDL_PATH}?limit=${CONFIG.DAYS}&auth_token=${CONFIG.QUANDL_TOKEN}`,
   }
 
   let body = '';
@@ -72,32 +75,18 @@ const vixParser = () => new Promise((resolve, reject) => {
 
     res.on('end', () => {
       let vixJson = JSON.parse(body);
-      let element = '';
 
-      vix.set("Source", "CBOE VIX");
       const vixSeries = {};
-      const labels = [];
-      const data = [];
+      vixSeries.labels = [];
+      vixSeries.data = [];
+
       // Parse vix series
-      // i=0:Date, i=1:VIX Open, i=2:VIX High, i=3:VIX Low, i=4:VIX Close
       const DATE_INDEX = 0, VIX_CLOSE_INDEX = 4;
-      vixJson.dataset_data.data.reverse().forEach((k, v) => {
-        labels.push(k[DATE_INDEX]);
-        data.push(k[VIX_CLOSE_INDEX]);
-      });
-      vixSeries.labels = labels;
-      vixSeries.data = data;
-      // console.log(vixSeries);
-      // pic VIX data
       // i=0:Date, i=1:VIX Open, i=2:VIX High, i=3:VIX Low, i=4:VIX Close
-      vixJson.dataset_data.column_names.forEach((v, i) => {
-        // console.log(`v: ${v} -> ${i}`);
-        element = vixJson.dataset_data.data[0][i];
-        // console.log(`${v} -> Element:${element}`);
-        vix.set(v, element);
+      vixJson.dataset_data.data.reverse().forEach((k, v) => {
+        vixSeries.labels.push(k[DATE_INDEX]);
+        vixSeries.data.push(k[VIX_CLOSE_INDEX]);
       });
-      // console.log("------" , vix);
-      // resolve(vix);
       resolve(vixSeries);
     });
   })
@@ -110,61 +99,19 @@ const vixParser = () => new Promise((resolve, reject) => {
   req.end();
 });
 
-// const postToSlack = (vixMap) => new Promise((resolve, reject) => {
-
-const postToSlack = (vixPng) => new Promise((resolve, reject) => {
-  // Transfer for slack post format
-  // const o = {};
-  // o.text = '';
-
-  // for (let [k, v] of vixMap.entries()) {
-  //   o.text += `${k}:${v} `;
-  // }
-  // const vix = JSON.stringify(o);
-  // console.log(vix);
-
-  // const options = {
-  //   hostname: TOKEN.SLACK_HOST,
-  //   path: TOKEN.SLACK_PATH,
-  //   method: 'POST',
-  //   headers: {
-  //     // 'Content-Type': 'application/json',
-  //     // 'Content-Length': vix.length
-  //     'Content-Type': 'image/png',
-  //     'Content-Length': vixPng.length
-  //   }
-  // }
-
-  // const req = https.request(options, (res) => {
-  //   console.log('STATUS: ' + res.statusCode);
-  //   console.log('HEADERS: ' + JSON.stringify(res.headers));
-  //   res.setEncoding('utf8');
-  //   res.on('data', (chunk) => {
-  //     console.log('BODY: ' + chunk);
-  //   });
-
-  //   res.on('end', () => {
-  //     resolve('Post to Slack done.');
-  //   });
-  // });
-
-  // req.on('error', (e) => {
-  //   console.log('problem with request: ' + e.message);
-  // });
-
-  // req.write(vixPng);
-
-  // req.end();
+const postToSlack = (vixPng, todayVIX) => new Promise((resolve, reject) => {
   const web = new WebClient(CONFIG.SLACK_TOKEN);
-  const channel = 'D8A9Z0DB2';
+  const channel = CONFIG.SLACK_CHANNEL;
   const option = {
     channels: channel,
     file: vixPng,
-    filename: 'VIX graph for the last two weeks.',
+    filename: `CBOE VIX trend`,
+    filetype: 'png',
+    initial_comment: `Last ${CONFIG.DAYS} days VIX, Today VIX is ${todayVIX}`,
   }
 
   web.files.upload(option)
     .then(res => {
-      console.log('');
+      console.log(`Post file [ ${res.file.name} ] done`);
     }).catch(console.error);
 });
